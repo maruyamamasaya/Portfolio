60560acf7bc9663a1f480d228c351eac4fd03600
maruma <maruyama_001@jimusuru.info>
2025-07-25 13:20:50 +0900

    Remove SocialLinks from blog sidebar

--- src/app/components/RightSidebar.tsx
+++ src/app/components/RightSidebar.tsx
@@ -1,6 +1,5 @@
 import RecentPosts from './RecentPosts';
 import TagList from './TagList';
-import SocialLinks from './SocialLinks';
 import TableOfContents, { Heading } from './TableOfContents';
 import { Post } from '@/lib/posts';
 
@@ -25,9 +24,6 @@ export default function RightSidebar({ posts, headings }: Props) {
       </div>
       <div className="widget">
         <TagList />
-      </div>
-      <div className="widget">
-        <SocialLinks />
       </div>
     </div>
   );
