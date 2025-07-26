bee1f976fc8398f7d2a8a742a2ffd679e5ab54d2
maruma <maruyama_001@jimusuru.info>
2025-07-26 13:44:16 +0900

    Remove unused Processor import

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,4 +1,4 @@
-import { unified, type Plugin, type Processor } from 'unified';
+import { unified, type Plugin } from 'unified';
 import parse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeSlug from 'rehype-slug';
@@ -105,7 +105,7 @@ export default async function markdownToHtml(
     convertMarkdownTables(replaceInternalLinks(markdown))
   );
 
-  const processor: Processor = unified()
+  const processor = unified()
     .use(parse)
     .use(headingsPlugin(headings))
     .use(remarkRehype)
