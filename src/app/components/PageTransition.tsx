bd1b12757a876e14d614d33883710091e2dbe661
maruma <maruyama_001@jimusuru.info>
2025-07-26 13:27:16 +0900

    Add toggle for initial animation

--- src/app/components/PageTransition.tsx
+++ src/app/components/PageTransition.tsx
@@ -5,10 +5,15 @@ import { usePathname } from "next/navigation";
 
 export default function PageTransition({ children }: { children: ReactNode }) {
   const pathname = usePathname();
+  const disable = process.env.NEXT_PUBLIC_DISABLE_INITIAL_ANIMATION === "true";
+  if (disable) {
+    return <>{children}</>;
+  }
   return (
     <AnimatePresence mode="wait" initial={false}>
       <motion.div
         key={pathname}
+        className="gpu-optimize"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
