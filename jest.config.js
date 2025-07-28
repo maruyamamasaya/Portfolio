ce4576bc3db5972827a451f2f6d2da0e12c3435c
maruma <maruyama_001@jimusuru.info>
2025-07-28 15:20:32 +0900

    Configure Jest for ESM modules and add sample posts

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!unified|remark-parse|remark-rehype|rehype-slug)/',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
