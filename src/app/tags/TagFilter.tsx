fc870c0b47744e66d483af7e9e7d24b473557c14
maruma <maruyama_001@jimusuru.info>
2025-07-30 01:19:33 +0900

    Increase blog card image size

--- src/app/tags/TagFilter.tsx
+++ src/app/tags/TagFilter.tsx
@@ -77,9 +77,9 @@ export default function TagFilter({ tags, posts }: Props) {
                 <Image
                   src={post.image}
                   alt={`Thumbnail for ${post.title}`}
-                  width={64}
-                  height={64}
-                  className="w-16 h-16 object-cover rounded-lg border shadow-sm"
+                  width={80}
+                  height={80}
+                  className="w-20 h-20 object-cover rounded-lg border shadow-sm"
                 />
               )}
               <div className="flex-1">
