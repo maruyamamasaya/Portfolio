065c1c369cd6e946018824b970fb8f20c4465a84
maruma <maruyama_001@jimusuru.info>
2025-08-01 02:52:54 +0900

    Simplify home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,5 @@ export const metadata = {
 
 export default async function HomePage() {
   const posts = await getSortedPosts();
-  return (
-    <div className="-mx-4 sm:-mx-6 md:-mx-8">
-      <HomeWindow posts={posts} />
-    </div>
-  );
+  return <HomeWindow posts={posts} />;
 }
