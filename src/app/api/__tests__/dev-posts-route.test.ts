8205bef9d60c20084a0123dec484a2c4048f1f5e
maruma <maruyama_001@jimusuru.info>
2025-08-09 16:27:39 +0900

    chore: remove developers blog api

--- src/app/api/__tests__/dev-posts-route.test.ts
+++
@@ -1,9 +0,0 @@
-import { GET } from '../dev-posts/route';
-
-describe('GET /api/dev-posts', () => {
-  it('returns dev post filenames', async () => {
-    const res = await GET();
-    const data = await res.json();
-    expect(data).toContain('dev-sample-post.md');
-  });
-});
