25c17ac6f29921d6daf94830911206487509047e
maruma <maruyama_001@jimusuru.info>
2025-08-17 00:46:34 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    modern: true,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
