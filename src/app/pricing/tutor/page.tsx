f359843ee2670d9663cb112de936134b4705b187
maruma <maruyama_001@jimusuru.info>
2025-08-20 22:20:02 +0900

    矢印画像を右向きに回転

--- src/app/pricing/tutor/page.tsx
+++ src/app/pricing/tutor/page.tsx
@@ -133,10 +133,7 @@ export default function TutorPricingPage() {
               </div>
               {idx < flowSteps.length - 1 && (
                 <div className="md:flex-shrink-0 flex flex-col items-center">
-                  <FlowArrow
-                    className="hidden md:block -rotate-90"
-                    alt="arrow"
-                  />
+                  <FlowArrow className="hidden md:block" alt="arrow" />
                   <FlowArrow className="md:hidden" alt="arrow" />
                 </div>
               )}
