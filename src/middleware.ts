b378c197ae9bdf0bef58a8134941f21493b8c1b7
maruma <maruyama_001@jimusuru.info>
2025-08-25 06:01:39 +0900

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('authorization');
+  const username = process.env.BASIC_AUTH_USERNAME ?? '';
+  const password = process.env.BASIC_AUTH_PASSWORD ?? '';
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    const [user, pwd] = atob(authValue).split(':');
+    if (user === username && pwd === password) {
+      return NextResponse.next();
+    }
+  }
+  return new NextResponse('Authentication required', {
+    status: 401,
+    headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+  });
+}
+
+export const config = {
+  matcher: ['/developer_edit', '/developer_edit/:path*'],
+};
